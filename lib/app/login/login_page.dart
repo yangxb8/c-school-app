5fc02ac30f66a6c87181ea12528a74c7d47a2d94
yangxb8 <yangxb10@gmail.com>
2020-12-11 22:08:16 +0900

    change login theme

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -5,7 +5,7 @@ import 'package:form_field_validator/form_field_validator.dart';
 import 'package:get/get.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'controller/login_controller.dart';
-import 'style/theme.dart' as theme;
+import 'style/login_theme.dart' as theme;
 import 'utils/bubble_indication_painter.dart';
 import '../../i18n/login_page.i18n.dart';
 
--- lib/app/login/style/login_theme.dart
+++ lib/app/login/style/login_theme.dart
@@ -1,15 +1,15 @@
 import 'dart:ui';
-
+import 'package:supercharged/supercharged.dart';
 import 'package:flutter/cupertino.dart';
 
 class Colors {
 
   const Colors();
 
-  static const Color loginGradientStart = Color(0xFFfbab66);
-  static const Color loginGradientEnd = Color(0xFFf7418c);
+  static final Color loginGradientStart = '#B5D0FA'.toColor();
+  static final Color loginGradientEnd = '#484C75'.toColor();
 
-  static const primaryGradient = LinearGradient(
+  static final primaryGradient = LinearGradient(
     colors: [loginGradientStart, loginGradientEnd],
     stops: [0.0, 1.0],
     begin: Alignment.topCenter,
