2c0d39039bfb9e3c195881f9991c2e7b4d79fb1c
FlutLab.io <>
2021-01-05 10:15:27 +0000

    Switching to branch work/4

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,11 @@ import 'dart:io';
 import 'package:c_school_app/service/lecture_service.dart';
 import 'package:c_school_app/service/user_service.dart';
 import 'package:catcher/catcher.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:keyboard_dismisser/keyboard_dismisser.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'package:wiredash/wiredash.dart';
 import 'package:splashscreen/splashscreen.dart';
-
 import './service/app_state_service.dart';
-
 import 'app_theme.dart';
 import 'controller/ui_view_controller/speech_recording_controller.dart';
 import 'router.dart';
@@ -28,14 +26,18 @@ void main() async {
   await SystemChrome.setPreferredOrientations(<DeviceOrientation>[
     DeviceOrientation.portraitUp,
   ]);
+
   /// Debug configuration with dialog report mode and console handler. It will show dialog and once user accepts it, error will be shown   /// in console.
-  var debugOptions =
-      CatcherOptions(SilentReportMode(), [ConsoleHandler()]);
+  var debugOptions = CatcherOptions(SilentReportMode(), [ConsoleHandler()]);
+
   /// Release configuration. Same as above, but once user accepts dialog, user will be prompted to send email with crash to support.
   var releaseOptions = CatcherOptions(DialogReportMode(), [
     EmailManualHandler(['yangxb10@gmail.com'])
   ]);
-  Catcher(rootWidget: CSchoolApp(), debugConfig: debugOptions, releaseConfig: releaseOptions);
+  Catcher(
+      rootWidget: CSchoolApp(),
+      debugConfig: debugOptions,
+      releaseConfig: releaseOptions);
 }
 
 class CSchoolApp extends StatelessWidget {
@@ -52,47 +54,57 @@ class CSchoolApp extends StatelessWidget {
     ));
     return KeyboardDismisser(
         child: Wiredash(
-          projectId: 'c-school-iysnrje',
-          secret: 'rbl6r14rthdvtkruhfu0lvlldp6rpq3pepclnowm1q6ui08u',
-          /// We use Catcher's navigatorKey here also for Wiredash
-          navigatorKey: Catcher.navigatorKey,
-          child: ScreenUtilInit(
-            designSize: Size(1080, 2220),
-            child: GetMaterialApp(
-                title: 'CSchool',
-                debugShowCheckedModeBanner: false,
-                defaultTransition: Transition.fade,
-                navigatorKey: Catcher.navigatorKey,
-                theme: ThemeData(
-                  primarySwatch: Colors.blue,
-                  textTheme: AppTheme.textTheme,
-                  platform: TargetPlatform.iOS,
-                ),
-                localizationsDelegates: [
-                  GlobalMaterialLocalizations.delegate,
-                  GlobalWidgetsLocalizations.delegate,
-                  GlobalCupertinoLocalizations.delegate,
-                ],
-                supportedLocales: [
-                  const Locale('en'),
-                  const Locale('ja', 'JP'),
-                ],
-                getPages: AppRouter.setupRouter(),
-                home: Splash(),
-            ),
-          ),
-        ));
+      projectId: 'c-school-iysnrje',
+      secret: 'rbl6r14rthdvtkruhfu0lvlldp6rpq3pepclnowm1q6ui08u',
+
+      /// We use Catcher's navigatorKey here also for Wiredash
+      navigatorKey: Catcher.navigatorKey,
+      child: GetMaterialApp(
+        builder: (context, widget) => ResponsiveWrapper.builder(
+          BouncingScrollWrapper.builder(context, widget),
+          maxWidth: 1200,
+          minWidth: 450,
+          defaultScale: true,
+          breakpoints: [
+            ResponsiveBreakpoint.resize(450, name: MOBILE),
+            ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+          ]),
+        title: 'CSchool',
+        debugShowCheckedModeBanner: false,
+        defaultTransition: Transition.fade,
+        navigatorKey: Catcher.navigatorKey,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          textTheme: AppTheme.textTheme,
+          platform: TargetPlatform.iOS,
+        ),
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          const Locale('en'),
+          const Locale('ja', 'JP'),
+        ],
+        getPages: AppRouter.setupRouter(),
+        home: Splash(),
+      ),
+    ));
   }
 }
 
 class Splash extends StatelessWidget {
-    void navigateToHome() async{
-        await Get.toNamed(UserService.user.isLogin() ? '/review/words/home' : '/login');
-    }
+  void navigateToHome() async {
+    await Get.toNamed(
+        UserService.user.isLogin() ? '/review/words/home' : '/login');
+  }
+
   Future<void> _loadFromFuture() async {
     await initServices();
     // TODO: Only for development, might need a proper way to upload our class
-    if(AppStateService.isDebug){
+    if (AppStateService.isDebug) {
       await Get.find<ApiService>().firestoreApi.uploadLecturesByCsv();
       await Get.find<ApiService>().firestoreApi.uploadWordsByCsv();
     }
@@ -102,10 +114,11 @@ class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SplashScreen(
-        navigateAfterFuture: _loadFromFuture(),
-        imageBackground: Image.asset('assets/splash/splash.png').image,
-        photoSize: 100.0.r,
-        useLoader: false,);
+      navigateAfterFuture: _loadFromFuture(),
+      imageBackground: Image.asset('assets/splash/splash.png').image,
+      photoSize: 100.0,
+      useLoader: false,
+    );
   }
 }
 
@@ -118,6 +131,5 @@ Future<void> initServices() async {
   Get.lazyPut<SpeechRecordingController>(() => SpeechRecordingController());
   await Get.putAsync<LectureService>(
       () async => await LectureService.getInstance());
-  Logger.level =
-      AppStateService.isDebug ? Level.debug : Level.error;
+  Logger.level = AppStateService.isDebug ? Level.debug : Level.error;
 }
