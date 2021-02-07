bf185c8f8f7ebb0c9565a8e58e5c49fcc8a33d84
yangxb8 <yangxb10@gmail.com>
2021-02-07 23:46:34 +0900

    fix login with google

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -63,74 +63,76 @@ class _LoginPageState extends State<LoginPage>
           onNotification: (overscroll) {
             overscroll.disallowGlow();
           },
-          child: ModalProgressHUD(
-              inAsyncCall: controller.processing.value,
-              child: SingleChildScrollView(
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height >= 775.0
-                      ? MediaQuery.of(context).size.height
-                      : 775.0,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        colors: [
-                          theme.Colors.loginGradientStart,
-                          theme.Colors.loginGradientEnd
-                        ],
-                        begin: const FractionalOffset(0.0, 0.0),
-                        end: const FractionalOffset(1.0, 1.0),
-                        stops: [0.0, 1.0],
-                        tileMode: TileMode.clamp),
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(top: 85.0),
-                        child: Image(
-                            width: 250.0,
-                            height: 191.0,
-                            fit: BoxFit.fill,
-                            image: AssetImage('assets/login/login_logo.png')),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 20.0),
-                        child: _buildMenuBar(context),
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: PageView(
-                          controller: _pageController,
-                          onPageChanged: (i) {
-                            if (i == 0) {
-                              setState(() {
-                                right = Colors.white;
-                                left = Colors.black;
-                              });
-                            } else if (i == 1) {
-                              setState(() {
-                                right = Colors.black;
-                                left = Colors.white;
-                              });
-                            }
-                          },
-                          children: <Widget>[
-                            ConstrainedBox(
-                              constraints: const BoxConstraints.expand(),
-                              child: _buildSignIn(context),
-                            ),
-                            ConstrainedBox(
-                              constraints: const BoxConstraints.expand(),
-                              child: _buildSignUp(context),
-                            ),
+          child: Obx(()=>
+            ModalProgressHUD(
+                inAsyncCall: controller.processing.value,
+                child: SingleChildScrollView(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height >= 775.0
+                        ? MediaQuery.of(context).size.height
+                        : 775.0,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [
+                            theme.Colors.loginGradientStart,
+                            theme.Colors.loginGradientEnd
                           ],
+                          begin: const FractionalOffset(0.0, 0.0),
+                          end: const FractionalOffset(1.0, 1.0),
+                          stops: [0.0, 1.0],
+                          tileMode: TileMode.clamp),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(top: 85.0),
+                          child: Image(
+                              width: 250.0,
+                              height: 191.0,
+                              fit: BoxFit.fill,
+                              image: AssetImage('assets/login/login_logo.png')),
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: EdgeInsets.only(top: 20.0),
+                          child: _buildMenuBar(context),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: PageView(
+                            controller: _pageController,
+                            onPageChanged: (i) {
+                              if (i == 0) {
+                                setState(() {
+                                  right = Colors.white;
+                                  left = Colors.black;
+                                });
+                              } else if (i == 1) {
+                                setState(() {
+                                  right = Colors.black;
+                                  left = Colors.white;
+                                });
+                              }
+                            },
+                            children: <Widget>[
+                              ConstrainedBox(
+                                constraints: const BoxConstraints.expand(),
+                                child: _buildSignIn(context),
+                              ),
+                              ConstrainedBox(
+                                constraints: const BoxConstraints.expand(),
+                                child: _buildSignUp(context),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              )),
+                ),
+          )),
     );
   }
 
